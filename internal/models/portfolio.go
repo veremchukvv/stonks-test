a318afe55ad1b17cc4b14fb4530f29f86e4d09e7
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-10-04 22:27:31 +0300

    add few models

---
+++ internal/models/portfolio.go
@@ -0,0 +1,9 @@
+package models
+
+type Portfolio struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Public bool `json:"public"`
+	Stocks map[Stock]int `json:"stocks"`
+	Cash float32 `json:"cash"`
+}

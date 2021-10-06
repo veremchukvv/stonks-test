d34c114ee02924293cf90b7572c529b5977ff181
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-10-06 23:12:44 +0300

    started handlers implementation

---
+++ internal/handler/login.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func (h *Handler) login(c *echo.Context) {
+
+}
+
+func (h *Handler) logout(c *echo.Context) {
+
+}
---
+++ internal/handler/profile.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func (h *Handler) profile(c *echo.Context) {
+
+}

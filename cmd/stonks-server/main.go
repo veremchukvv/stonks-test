72f512b12b76afaca01fe2964d58643d3779c08a
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-10-17 14:17:22 +0300

    moved server to internal dir

--- cmd/stonks-server/main.go
+++ cmd/stonks-server/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/veremchukvv/stonks-test/internal/handler"
-	"github.com/veremchukvv/stonks-test/restapi"
+	"github.com/veremchukvv/stonks-test/internal/api-server"
 	"log"
 )
 
 func main() {
-	srv := new(restapi.Server)
+	srv := new(api_server.Server)
 	handlers := new(handler.Handler)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while server is started: %s", err.Error())
--- internal/api-server/server.go
+++ internal/api-server/server.go
@@ -1,4 +1,4 @@
-package restapi
+package api_server
 
 import (
 	"context"

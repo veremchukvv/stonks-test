f20043e65e719c0bc5274fed8e177d204335afde
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-10-18 23:09:00 +0300

    added configuration with cleanenv

--- cmd/stonks-server/main.go
+++ cmd/stonks-server/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"github.com/veremchukvv/stonks-test/internal/api-server"
+	"github.com/veremchukvv/stonks-test/internal/config"
 	"github.com/veremchukvv/stonks-test/internal/handlers"
 	"github.com/veremchukvv/stonks-test/pkg/logging"
-	"go.uber.org/zap"
 )
 
 func main() {
+
+	cfg := config.GetConfig()
+
 	logger := logging.NewLogger(false, "console")
-	zap.ReplaceGlobals(logger)
 	srv := new(api_server.Server)
 	handler := new(handlers.Handler)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
-		zap.S().Fatalf("error occured while server is started: %s", err.Error())
+	logger.Info("Handlers initialized")
+	logger.Infof("Server running on port: %s", cfg.Server.Port)
+	if err := srv.Run(cfg.Server.Port, handler.InitRoutes()); err != nil {
+		logger.Fatalf("error occured while server is started: %s", err.Error())
 	}
+
 }

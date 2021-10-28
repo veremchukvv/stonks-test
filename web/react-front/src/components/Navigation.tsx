7ca7c2a67e263d4bbf8d93e81a9f4f906fdd435d
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-10-28 23:24:24 +0300

    added react router

--- web/react-front/src/components/Navigation.tsx
+++ web/react-front/src/components/Navigation.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Home</a>
+                    <Link to="/" className="navbar-brand">Home</Link>
                     <div>
                         <ul className="navbar-nav me-auto mb-2 mb-md-0">
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Login</a>
+                                <Link to="/login" className="nav-link">Login</Link>
                             </li>
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Register</a>
+                                <Link to="/register" className="nav-link">Register</Link>
                             </li>
                         </ul>
                     </div>

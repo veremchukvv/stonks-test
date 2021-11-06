6e2acfe9d7a68d882d1bed6263e4fe2866c25960
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-11-06 19:47:49 +0300

    home page upgraded

--- web/react-front/src/pages/Home.tsx
+++ web/react-front/src/pages/Home.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/users/user', {
+                    headers: {'Content-Type':'application/json'},
+                    })
+                const content = await response.json()
+
+                setName(content.name)
+
+                }
+        )()
+    })
+
     return (
         <div>
-            Home
+            Welcome {name} !
         </div>
     );
 };
--- web/react-front/src/pages/Login.tsx
+++ web/react-front/src/pages/Login.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {SyntheticEvent, useState} from 'react';
+import { Redirect } from 'react-router-dom';
 import googleLogo from "../assets/google_logo.png"
 import vkLogo from "../assets/vk_logo.png"
 
@@ -11,6 +12,30 @@ const vkLogin = () => {
 }
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault()
+
+        await fetch('http://localhost:8000/users/signin', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        setRedirect(true)
+    }
+
+    if (redirect)
+    {
+        return <Redirect to="/home"/>
+    }
+
     return (
         <div className="loginPage">
                 <h1 className="h3 mb-3 fw-normal">Please sign in via third party using OAuth</h1>
@@ -25,9 +50,13 @@ const Login = () => {
                     </div>
                 </div>
             <h1 className="h3 mb-3 fw-normal">Or using local credentials</h1>
-            <form>
-                <input type="email" className="form-control" placeholder="name@example.com" />
-                <input type="password" className="form-control" placeholder="Password" />
+            <form onSubmit={submit}>
+                <input type="email" className="form-control" placeholder="name@example.com"
+                       onChange = {e => setEmail(e.target.value)}
+                />
+                <input type="password" className="form-control" placeholder="Password"
+                       onChange = {e => setPassword(e.target.value)}
+                />
                 <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
             </form>
         </div>

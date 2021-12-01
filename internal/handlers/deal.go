34868c3e9a16f6c255f9a618eb139c80aa1ce6f6
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-12-01 22:29:45 +0300

    deals api started

---
+++ internal/handlers/deal.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func (h *Handler) getOneDeal(c echo.Context) error {
+	return c.String(http.StatusNotImplemented, "not implemented yet")
+}
+
+func (h *Handler) closeDeal(c echo.Context) error {
+	return c.String(http.StatusNotImplemented, "not implemented yet")
+}
+
+func (h *Handler) deleteDeal(c echo.Context) error {
+	return c.String(http.StatusNotImplemented, "not implemented yet")
+}
+
+
+
